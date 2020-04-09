728e7de62d658cc420669ffeb89bd3fa024c6b7e
bagshik <bagshik@gmail.com>
2020-04-09 17:25:27 +0200

    Replaced default route with "/login"

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login')->name('login')->uses('Auth\LoginController')->middleware('guest');
